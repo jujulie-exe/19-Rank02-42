4ac373fbd3061a526c208320f7ef35027e2e7a7e
lilith_exe <jujulie@juju.exe>
2024-12-12 11:49:15 +0100

    INVALID_HEADER

--- 03_minitalk/client.c
+++ 03_minitalk/client.c
@@ -1,6 +1,6 @@
 #include "mini_talk.h"
 
-volatile sig_atomic_t g_server_reponse = BUSY;
+volatile sig_atomic_t	g_server_reponse = BUSY;
 
 void	end_message(int sig)
 {
@@ -13,41 +13,42 @@ void	ready_or_not(int sig)
 	g_server_reponse = READY;
 }
 
-void send_message(pid_t server_pid, char message) 
+void	send_message(pid_t server_pid, char message)
 {
-    int bit; 
-    int bit_value;
+	int	bit;
+	int	bit_value;
 
-    bit = 0;
-    while (bit < CHAR_BIT) 
-    { 
-        bit_value = (message >> bit) & 1;
-        if (bit_value == 0) 
-            ft_kill_check(server_pid, SIGUSR1); 
-         else 
-            ft_kill_check(server_pid, SIGUSR2); 
-        bit++; 
-	while (g_server_reponse == BUSY)
-		usleep(45); 
-	g_server_reponse = BUSY;
-    }
+	bit = 0;
+	while (bit < CHAR_BIT)
+	{
+		bit_value = (message >> bit) & 1;
+		if (bit_value == 0)
+			ft_kill_check(server_pid, SIGUSR1);
+		else
+			ft_kill_check(server_pid, SIGUSR2);
+		bit++;
+		while (g_server_reponse == BUSY)
+			usleep(45);
+		g_server_reponse = BUSY;
+	}
 }
 
-int main(int argc, char *argv[]) 
+int	main(int argc, char **argv)
 {
-    if (argc != 3) 
-    {
-        write(1,"Usage: <server_pid> <message>\n", 30);
-        return 1;
-    }
+	const char	*message;
+	pid_t		server_pid;
 
-    pid_t server_pid = atoi(argv[1]); 
-    const char *message = argv[2];
-    ft_siganl(SIGUSR2, ready_or_not, false);
-    ft_siganl(SIGUSR1, end_message, false);
-    while (*message) 
-        send_message(server_pid, *message++);
-    send_message(server_pid, '\0');
-    return 0;
+	if (argc != 3)
+	{
+		write(1, "Usage: <server_pid> <message>\n", 30);
+		return (1);
+	}
+	server_pid = atoi(argv[1]);
+	message = argv[2];
+	ft_siganl(SIGUSR2, ready_or_not, false);
+	ft_siganl(SIGUSR1, end_message, false);
+	while (*message)
+		send_message(server_pid, *message++);
+	send_message(server_pid, '\0');
+	return (0);
 }
-
